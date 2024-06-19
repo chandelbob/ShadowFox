6682a79767817e802545b905791fa605cb9f8d2c
chandelbob <ayushbr76@gmail.com>
2024-06-19 21:39:48 +0530

    variable task

---
+++ Task 1/variable.py
@@ -0,0 +1,32 @@
+#Create a variable named pi and store the value 22/7 in it.
+# Create a variable named pi and store the value 22/7 in it.
+# Now check the data type of this variable.
+
+
+pi = 22/7
+print(type(pi))
+
+#<class 'float'>
+
+#. Create a variable called for and assign it a value 4.
+#  See whathappens and find out the reason behind the behavior that you see.
+
+
+#for = 4
+    
+#syntax error : invalid syntax as for is a reserved key wor
+'''
+Store the principal amount, rate of interest, and time in
+different variables and then calculate the Simple Interest for 3
+years. Formula: Simple Interest = P x R x T / 100
+
+'''
+p = 100
+r = 9
+t = 3
+
+formula = (p * r * t )/100
+
+print(formula)
+
+# simple intrest = 27
