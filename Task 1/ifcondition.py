747d999f2c25effdc9ded5b0f9b411432a91443f
chandelbob <ayushbr76@gmail.com>
2024-06-23 11:53:24 +0530

    if elif else

--- Task 1/ifcondition.py
+++ Task 1/ifcondition.py
@@ -34,4 +34,61 @@ def bmi():
 
 bmi()                   # Calling Funtion 
 
+#------------------------------------------------------------
+
+# 2. Write a program to determine which country a city belongs to. Given list of cities per country:
+# Australia = ["Sydney", "Melbourne", "Brisbane", "Perth"]
+# UAE = ["Dubai", "Abu Dhabi", "Sharjah", "Ajman"]
+# India = ["Mumbai", "Bangalore", "Chennai", "Delhi"]
+# Ask the user to enter a city name and print the corresponding country.
+# Example:
+# Enter a city name: "Abu Dhabi" 
+# Output: "Abu Dhabi is in UAE"
+
+Australia = ["sydney", "melbourne", "brisbane", "perth"]
+UAE = ["dubai", "abu dhabi", "sharjah", "ajman"]
+India = ["mumbai", "bangalore", "chennai", "delhi"]
+
+city=input("city name:")
+city_name = city.lower()
+
+#if else loop start
+
+if city_name in Australia:
+    print(f"{city_name} is in australia")
+elif city_name  in UAE:
+    print (f"{city_name} is in UAE")
+elif city_name in India:
+    print (f"{city_name}is in india")
+else:
+    print("no info")
+#-------------------------------------------------------------------------------------
+# 3. Write a program to check if two cities belong to the same country. 
+# Ask the user to enter two cities and print whether they belong to the same country or not.
+# Example:
+# Enter the first city: "Mumbai"
+# Enter the second city: "Chennai"
+# Output: "Both cities are in India"
+# Example:
+# Enter the first city: "Sydney"
+# Enter the second city: "Dubai"
+# Output: "They don't belong to the same country"
+# '''
+city1_name=input("1st city name:") 
+city2_name=input("2nd city name")
+city1_name = city1_name.lower()
+city2_name = city2_name.lower()
+
+#if else loop start
+if city1_name in Australia and city2_name in Australia:
+    print(f"{city1_name} and {city2_name} are in austrlia")
+elif city1_name in UAE and city2_name in UAE :                         # Checking if the city names comes under the UAE List
+    print("Both cities are in UAE")
+elif city1_name in India and city2_name in India :                     # Checking if the city names comes under the India List
+    print ("Both cities are in India")
+else :
+    print ("They don't belong to the same country")      # Wrong Input Handling
+
+
+
 
